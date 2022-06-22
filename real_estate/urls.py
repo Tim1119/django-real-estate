29b95e2c94bd0a633b8f8da5f1af1957f08cf78e
tim1119 <timothyinioluwa@gmail.com>
2022-06-22 23:57:57 +0100

    feat:user auth-register,login,password reset

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    # ---Change the url to super secret before deployment
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
     path('admin/', admin.site.urls),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
